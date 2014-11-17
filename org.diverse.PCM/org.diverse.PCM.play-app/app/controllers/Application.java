f3d6af9b0b913ad803d1eeaa9258b37cd2559e24
gbecan <guillaume.becan@irisa.fr>
2014-11-17 17:09:05 +0100

    preparing website

--- org.diverse.PCM/org.diverse.PCM.play-app/app/controllers/Application.java
+++ org.diverse.PCM/org.diverse.PCM.play-app/app/controllers/Application.java
@@ -1,12 +1,19 @@
 package controllers;
 
+import play.api.libs.json.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.ArrayList;
 
 public class Application extends Controller {
     
     public static Result index() {
-        return ok(views.html.index.render("Compare.io...what else?"));
+        return ok(views.html.index.render("OpenCompare...what else?"));
+    }
+
+    public static Result list() {
+        return ok(Json.toJson(new ArrayList<String>()));
     }
     
 }
---
+++ org.diverse.PCM/org.diverse.PCM.play-app/public/javascripts/controllers/pcm-list-controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by gbecan on 16/10/14.
+ */
+
+var pcmApp = angular.module("pcmApp", []);
+
+pcmApp.controller("PCMListController", function($scope) {
+    $scope.pcmList = ["1" , "2" , "3"]
+    $.ajax
+});
