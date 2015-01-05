e0e48e2039ae33a04ea122a010fdcabf711369de
Stéphane Mangin <stephane.mangin@freesbee.fr>
2015-01-05 21:32:50 +0100

    Sprint 1: Model (Product, cell, feature) unit test (Unstable, only for test purpose)

---
+++ org.diverse.PCM/org.diverse.PCM.io.Wikipedia/src/test/java/org/diverse/pcm/io/wikipedia/ModelTest.java
@@ -0,0 +1,49 @@
+package org.diverse.pcm.io.wikipedia;
+
+import org.diverse.pcm.api.java.impl.*;
+import org.diverse.pcm.api.java.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ModelTest {
+
+    PCMFactoryImpl factory = new PCMFactoryImpl();
+    Product product;
+    Feature feature;
+    Cell cell;
+    Value value;
+
+    @Before
+    public void setUp() throws Exception {
+        feature = factory.createFeature();
+        feature.setName("Feature1");
+        value = factory.createBooleanValue();
+        cell = factory.createCell();
+        cell.setInterpretation(value);
+        cell.setFeature(feature);
+        cell.setContent("Value1");
+        product = factory.createProduct();
+        product.addCell(cell);
+        product.setName("Product1");
+    }
+
+    @Test
+    public void ProductValueTest() {
+        assertEquals(product.getName(), "Product1");
+        assertTrue(product.getCells().contains(cell));
+    }
+
+    @Test
+    public void CellValueTest() {
+        assertEquals(cell.getContent(), "Value1");
+        assertEquals(cell.getFeature(), feature);
+        assertEquals(cell.getInterpretation(), value);
+    }
+
+    @Test
+    public void FeatureValueTest() {
+        assertEquals(feature.getName(), "Feature1");
+    }
+}
