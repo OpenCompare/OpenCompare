8be452fce880622179e2c73ca1a76967b438822c
hvallee <hugo.v.vallee@gmail.com>
2015-01-09 23:18:04 +0100

    Add functions to acces files

---
+++ org.diverse.PCM/org.diverse.PCM.io.Wikipedia/src/test/java/org/diverse/pcm/io/wikipedia/FileFunctions.java
@@ -0,0 +1,72 @@
+package org.diverse.pcm.io.wikipedia;
+
+import org.diverse.pcm.io.wikipedia.pcm.Page;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.*;
+
+/* This is a class with useful function on files */
+public class FileFunctions {
+
+
+    static String readFile(String path, Charset encoding) throws IOException {
+
+        StringBuilder string = new StringBuilder();
+
+        BufferedReader buff = new BufferedReader(new FileReader(path));
+        try {
+            String line;
+            while ((line = buff.readLine()) != null) {
+                string.append(line);
+            }
+        } finally {
+            buff.close();
+        }
+
+        return string.toString();
+        /*
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    */
+    }
+
+    public static void appendToFile(Exception e, String title) {
+        try {
+            FileWriter fstream = new FileWriter("output/reporting/reporting.txt", true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            PrintWriter pWriter = new PrintWriter(out, true);
+            pWriter.write("\n\n"+title+"\n");
+            e.printStackTrace(pWriter);
+        }
+        catch (Exception ie) {
+            throw new RuntimeException("Could not write Exception to file", ie);
+        }
+    }
+
+    public static void appendToFile1(Exception e, String title) {
+        try {
+            FileWriter fstream = new FileWriter("output/reporting/reporting1.txt", true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            PrintWriter pWriter = new PrintWriter(out, true);
+            pWriter.write("\n\n"+title+"\n");
+            e.printStackTrace(pWriter);
+        }
+        catch (Exception ie) {
+            throw new RuntimeException("Could not write Exception to file", ie);
+        }
+    }
+
+    public static void writeToPreprocessed(String preprocessed, String title) {
+        try {
+            FileWriter fstream = new FileWriter("output/preprocessed/"+title+".txt", true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            PrintWriter pWriter = new PrintWriter(out, true);
+            pWriter.write(preprocessed);
+        }
+        catch (Exception ie) {
+            throw new RuntimeException("Could not write Exception to file", ie);
+        }
+    }
+
+}
