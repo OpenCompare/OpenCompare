69e13070df69daa5fc831bc7cb6a2741f3fbed1c
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-04-27 17:51:05 +0200

    fix formalizer test

--- org.opencompare/formalizer/src/test/scala/org/diverse/pcm/formalizer/CellContentInterpreterTest.scala
+++ org.opencompare/formalizer/src/test/scala/org/diverse/pcm/formalizer/CellContentInterpreterTest.scala
@@ -19,7 +19,7 @@ class CellContentInterpreterTest extends FlatSpec with Matchers {
 
   "CellContentInterpreter" should "interpret every cell in Wikipedia PCMs" in {
 
-    val path = "../org.diverse.PCM.io.Wikipedia/input/Comparison_of_AMD_processors.txt"
+    val path = "../io-wikipedia/input/Comparison_of_AMD_processors.txt"
     //val path = "../org.diverse.PCM.io.Wikipedia/input/Comparison_of_disk_encryption_software.txt"
 
     // Parse
