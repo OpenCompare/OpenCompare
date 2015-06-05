daf63c07ec2f3cf22a63c1a9f95e1bc17f95fd1d
StephaneMangin <1camel>
2015-06-05 11:53:58 +0200

    Bugfix circular tests

--- org.opencompare/api-java/src/test/scala/org/opencompare/api/java/CircularImportTest.scala
+++ org.opencompare/api-java/src/test/scala/org/opencompare/api/java/CircularImportTest.scala
@@ -5,6 +5,7 @@ import java.net.URL
 import org.opencompare.api.java.io.{PCMExporter, PCMLoader, CSVExporter, CSVLoader}
 import org.opencompare.api.java.util.SimplePCMElementComparator
 import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.prop.TableFor1
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
 import scala.io.Source
@@ -13,36 +14,39 @@ import scala.reflect.io.{File, Directory}
 /**
  * Created by smangin on 01/06/15.
  */
-abstract class CircularImportExportTest extends FlatSpec with Matchers with BeforeAndAfterAll {
-
-  // Inidicates a folder with a list of file to test concordingly to the import/export classes
-  val resource : URL
-  val pcmFactory : PCMFactory
-  val exporter : PCMExporter
+abstract class CircularImportTest(
+  val resource : URL,
+  val pcmFactory : PCMFactory,
+  val exporter : PCMExporter,
   val loader : PCMLoader
+   ) extends FlatSpec with Matchers with BeforeAndAfterAll {
 
-  def getResources: List[File] = {
+  var inputs : TableFor1[File] = _
+
+  private def getResources(): List[File] = {
     val file = new java.io.File(resource.getPath)
-    println(resource.getPath)
     val folder = new Directory(file)
     folder.files.filter(_.isFile).toList
   }
 
-  val inputs = Table(
-    ("Import test for " + resource.getPath),
-    getResources: _*
-  )
-
-  forAll(inputs) {
+  forAll(Table(
+    ("Import test"),
+    getResources(): _*
+  )) {
     (file: File) => {
       val name = file.stripExtension
-      val pcm1 = loader.load(Source.fromURI(file.toURI).mkString)
-      "A " + name + " PCM" should "be the same as the one created with it's " + exporter.getClass.getName + " representation" in {
+      "A " + name + " PCM" should "be the same as the one created with it's representation" in {
+        val pcm1 = loader.load(Source.fromURI(file.toURI).mkString)
+        pcm1.setName("Original")
+        pcm1.normalize(pcmFactory)
         val pcm2 = loader.load(exporter.export(pcm1))
-        pcm2.setName("From PCM1 Csv")
+        pcm2.normalize(pcmFactory)
+        pcm2.setName("From PCM1")
 
         var diff = pcm1.diff(pcm2, new SimplePCMElementComparator)
-        diff.hasDifferences shouldBe false
+        withClue(diff.toString) {
+          diff.hasDifferences shouldBe false
+        }
       }
     }
   }
