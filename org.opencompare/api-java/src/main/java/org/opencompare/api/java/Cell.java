cba5b574cb39cc27e74855839e7872b193a259bc
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-06-08 16:56:43 +0200

    implement raw content in java API

--- org.opencompare/api-java/src/main/java/org/opencompare/api/java/Cell.java
+++ org.opencompare/api-java/src/main/java/org/opencompare/api/java/Cell.java
@@ -4,6 +4,9 @@ public interface Cell extends PCMElement {
 
 	String getContent();
 	void setContent(String content);
+
+	String getRawContent();
+	void setRawContent(String content);
 	
 	Value getInterpretation();
 	void setInterpretation(Value interpretation);
