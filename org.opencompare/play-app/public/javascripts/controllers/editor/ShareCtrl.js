b69435a6564e1b32f327a6a23d7f6d9f431cd7ab
hvallee <hugo.v.vallee@gmail.com>
2015-06-30 15:31:13 +0200

    Twitter button

--- org.opencompare/play-app/public/javascripts/controllers/editor/ShareCtrl.js
+++ org.opencompare/play-app/public/javascripts/controllers/editor/ShareCtrl.js
@@ -10,13 +10,16 @@ pcmApp.controller("ShareCtrl", function($rootScope, $scope, $http, $timeout, uiG
     $scope.enableTitleOption = false;
     $scope.enableShareOption = false;
 
+    $scope.shareText = '';
+
     $scope.embedLink = '';
+    $scope.twitterLink = 'https://twitter.com/intent/tweet?text=%23opencompare&url=http://opencompare.org/pcm/'+id;
 
     $scope.updateEmbedLink = function() {
         $scope.embedLink = '<iframe src="http://'+window.location.hostname+':'+window.location.port+'/embedPCM/'+id
             +'?enableEdit='+$scope.enableEditOption+'&enableExport='+$scope.enableExportOption+'&enableTitle='+$scope.enableTitleOption+'&enableShare='+$scope.enableShareOption
             +'" scrolling="no"  width="100%" height="700px" style="border:none;"></iframe>';
-    }
+    };
 });
 
 
