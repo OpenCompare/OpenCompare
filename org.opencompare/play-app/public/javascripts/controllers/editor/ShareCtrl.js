2022fb90300607697adf0008323adbfe6072bb3c
hvallee <hugo.v.vallee@gmail.com>
2015-06-30 16:50:25 +0200

    Translate button at right

--- org.opencompare/play-app/public/javascripts/controllers/editor/ShareCtrl.js
+++ org.opencompare/play-app/public/javascripts/controllers/editor/ShareCtrl.js
@@ -14,8 +14,12 @@ pcmApp.controller("ShareCtrl", function($rootScope, $scope, $http, $timeout, uiG
 
     $scope.embedLink = '';
     $scope.twitterLink = 'https://twitter.com/intent/tweet?text=%23opencompare&url=http://opencompare.org/pcm/'+id;
+    $scope.facebookLink = 'https://www.facebook.com/sharer/sharer.php?u=http://opencompare.org/pcm/'+id;
+    $scope.emailLink = 'mailto:?body=http://opencompare.org/pcm/'+id;
+    $scope.googleLink = 'https://plus.google.com/share?url=http://opencompare.org/pcm/'+id;
+    $scope.redditLink = 'http://www.reddit.com/submit?url=http://opencompare.org/pcm/'+id;
 
-    $scope.updateEmbedLink = function() {
+    $scope.updateShareLinks = function() {
         $scope.embedLink = '<iframe src="http://'+window.location.hostname+':'+window.location.port+'/embedPCM/'+id
             +'?enableEdit='+$scope.enableEditOption+'&enableExport='+$scope.enableExportOption+'&enableTitle='+$scope.enableTitleOption+'&enableShare='+$scope.enableShareOption
             +'" scrolling="no"  width="100%" height="700px" style="border:none;"></iframe>';
