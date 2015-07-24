b5af504d572a7a5ac252de2271a1e246471224d8
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-07-24 18:18:03 +0200

    add empty java files to be able to publish non java projects to maven central

---
+++ org.opencompare/io-wikipedia/src/main/java/org/opencompare/io/wikipedia/NoJavaSources.java
@@ -0,0 +1,12 @@
+package org.opencompare.io.wikipedia;
+
+/**
+ * Created by gbecan on 7/24/15.
+ */
+public class NoJavaSources {
+
+    /**
+     * This class is here only to be able to publish Scala artefacts to Maven central.
+     */
+
+}
