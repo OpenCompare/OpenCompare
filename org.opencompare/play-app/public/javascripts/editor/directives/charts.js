e7d2d87c0305edb76f2e2abdcc8b7c1f126ea397
hvallee <hugo.v.vallee@gmail.com>
2015-08-12 09:23:07 +0200

    Charts in a template

---
+++ org.opencompare/play-app/public/javascripts/editor/directives/charts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hvallee on 6/19/15.
+ */
+
+    pcmApp.directive('openCompareCharts', function() {
+        return {
+            templateUrl: '/assets/templates/charts.html'
+        };
+    });
