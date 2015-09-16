8478feafe93d9a42a86fdf6f4fdb8de3968a6c41
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-09-16 15:23:17 +0200

    use new editor with externalized pcm source

---
+++ org.opencompare/play-app/public/main/OCEditorCtrl.js
@@ -0,0 +1,25 @@
+/**
+ * Created by gbecan on 9/16/15.
+ */
+angular.module("openCompare")
+    .controller("OCEditorCtrl", function($scope, $modal) {
+
+        if (typeof id !== 'undefined') {
+            $scope.id = id;
+        }
+
+        if (typeof data !== 'undefined') {
+            $scope.data = data;
+        }
+
+        /* Load modal for import */
+        if (typeof modal != 'undefined') {
+            // Open the given modal
+            $modal.open({
+                templateUrl: "/templates/modal" + modal + ".html",
+                controller: modal + "Controller",
+                scope: $scope
+            })
+        }
+
+    });
