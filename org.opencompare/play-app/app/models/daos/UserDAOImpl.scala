41010f7da2ede46fedb1c81cf790b806fe54b06f
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-09-28 13:57:52 +0200

    save user information in database

--- org.opencompare/play-app/app/models/daos/UserDAOImpl.scala
+++ org.opencompare/play-app/app/models/daos/UserDAOImpl.scala
@@ -3,10 +3,11 @@ package models.daos
 import java.util.UUID
 
 import com.mohiva.play.silhouette.api.LoginInfo
-import models.User
+import com.mongodb.casbah.Imports._
 import models.daos.UserDAOImpl._
+import models.{Database, User}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-import scala.collection.mutable
 import scala.concurrent.Future
 
 /**
@@ -20,9 +21,20 @@ class UserDAOImpl extends UserDAO {
    * @param loginInfo The login info of the user to find.
    * @return The found user or None if no user for the given login info could be found.
    */
-  def find(loginInfo: LoginInfo) = Future.successful(
-    users.find { case (id, user) => user.loginInfo == loginInfo }.map(_._2)
-  )
+  def find(loginInfo: LoginInfo) = Future {
+//    users.find { case (id, user) => user.loginInfo == loginInfo }.map(_._2)
+    val request = MongoDBObject(
+      "providerID" -> loginInfo.providerID,
+      "providerKey" -> loginInfo.providerKey
+    )
+    val dbUser = users.findOne(request)
+    if (dbUser.isDefined) {
+      Some(loadFromDB(dbUser.get))
+    } else {
+      None
+    }
+  }
+
 
   /**
    * Finds a user by its user ID.
@@ -30,7 +42,15 @@ class UserDAOImpl extends UserDAO {
    * @param userID The ID of the user to find.
    * @return The found user or None if no user for the given ID could be found.
    */
-  def find(userID: UUID) = Future.successful(users.get(userID))
+  def find(userID: UUID) = Future {
+    val request = MongoDBObject("userID" -> userID.toString)
+    val dbUser = users.findOne(request)
+    if (dbUser.isDefined) {
+      Some(loadFromDB(dbUser.get))
+    } else {
+      None
+    }
+  }
 
   /**
    * Saves a user.
@@ -38,19 +58,39 @@ class UserDAOImpl extends UserDAO {
    * @param user The user to save.
    * @return The saved user.
    */
-  def save(user: User) = {
-    users += (user.userID -> user)
-    Future.successful(user)
+  def save(user: User) = Future {
+      users.insert(convertToDB(user))
+      user
+  }
+
+
+  private def convertToDB(user : User) : DBObject = {
+    MongoDBObject(
+      "userID" -> user.userID.toString,
+      "providerID" -> user.loginInfo.providerID,
+      "providerKey" -> user.loginInfo.providerKey,
+      "firstName" -> user.firstName,
+      "lastName" -> user.lastName,
+      "fullName" -> user.fullName,
+      "email" -> user.email,
+      "avatarURL" -> user.avatarURL
+    )
+  }
+
+  private def loadFromDB(dBObject: DBObject) : User = {
+    val userID = UUID.fromString(dBObject.get("userID").asInstanceOf[String])
+    val loginInfo = LoginInfo(dBObject.get("providerID").asInstanceOf[String], dBObject("providerKey").asInstanceOf[String])
+    val firstName = Option(dBObject.get("firstName").asInstanceOf[String])
+    val lastName = Option(dBObject.get("lastName").asInstanceOf[String])
+    val fullName = Option(dBObject.get("fullName").asInstanceOf[String])
+    val email = Option(dBObject.get("email").asInstanceOf[String])
+    val avatarURL = Option(dBObject.get("avatarURL").asInstanceOf[String])
+
+    User(userID, loginInfo, firstName, lastName, fullName, email, avatarURL)
   }
 }
 
-/**
- * The companion object.
- */
 object UserDAOImpl {
 
-  /**
-   * The list of users.
-   */
-  val users: mutable.HashMap[UUID, User] = mutable.HashMap()
+  val users = Database.db("users")
 }
