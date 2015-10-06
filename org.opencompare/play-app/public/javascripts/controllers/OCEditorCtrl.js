88a3ebdee96368973ecc2da85eecef7dfaf38e2a
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-10-06 15:11:40 +0200

    refactor editor

--- org.opencompare/play-app/public/javascripts/controllers/OCEditorCtrl.js
+++ org.opencompare/play-app/public/javascripts/controllers/OCEditorCtrl.js
@@ -2,15 +2,49 @@
  * Created by gbecan on 9/16/15.
  */
 angular.module("openCompare")
-    .controller("OCEditorCtrl", function($scope, $modal) {
+    .controller("OCEditorCtrl", function($rootScope, $scope, $modal, pcmApi, openCompareServer) {
+
+        openCompareServer.useLocalServer();
+
+        $scope.data = {
+            configuration: {
+                serverMode: "local"
+            }
+        };
 
         if (typeof id !== 'undefined') {
+            /* Load a PCM from database */
             $scope.id = id;
+            $scope.loading = true;
+            //$scope.setEdit(false, false);
+            //$scope.updateShareLinks();
+            openCompareServer.get("/api/get/" + $scope.id).
+                then(function (response) {
+                    var data = response.data;
+                    var pcm = pcmApi.loadPCMModelFromString(JSON.stringify(data.pcm));
+                    pcmApi.decodePCM(pcm); // Decode PCM from Base64
+
+                    $scope.data.pcm = pcm;
+                    $scope.data.metadata = data.metadata;
+                    $scope.data.id = id;
+                    $scope.state = {
+                        saved: true
+                    };
+
+                }, function(error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                })
         }
 
+
+
         if (typeof data !== 'undefined') {
-            $scope.data = data;
-            $scope.pcmContainer = JSON.stringify(JSON.parse($scope.data).pcm); // FIXME : mmhh ugly...
+            var pcm = pcmApi.loadPCMModelFromString(JSON.stringify(JSON.parse(data).pcm)); // FIXME : mmhh ugly...
+            pcmApi.decode(pcm);
+            $scope.data.pcm = pcm;
         }
 
         /* Load modal for import */
@@ -22,5 +56,7 @@ angular.module("openCompare")
                 scope: $scope
             })
         }
+
+
 
     });
