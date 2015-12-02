c84d55288487c03d24c9456339fc32940797571b
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-12-02 16:21:14 +0100

    use ids for key of positions + add test for KMFJSON format

--- org.opencompare/api-java-impl/src/main/scala/org/opencompare/api/java/impl/io/KMFJSONExporter.scala
+++ org.opencompare/api-java-impl/src/main/scala/org/opencompare/api/java/impl/io/KMFJSONExporter.scala
@@ -1,10 +1,12 @@
 package org.opencompare.api.java.impl.io
 
-import org.opencompare.api.java.PCMContainer
-import org.opencompare.api.java.impl.PCMImpl
+import org.opencompare.api.java.{Product, Feature, PCMContainer}
+import org.opencompare.api.java.impl.{FeatureImpl, ProductImpl, PCMImpl}
 import org.opencompare.api.java.io.PCMExporter
 import org.opencompare.model.pcm.factory.DefaultPcmFactory
-import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.json._
+
+import collection.JavaConversions._
 
 /**
  * Created by gbecan on 13/10/14.
@@ -20,32 +22,61 @@ class KMFJSONExporter(val base64Encoding : Boolean = true) extends PCMExporter {
   private val encoder = new PCMBase64Encoder
 
   override def export(container: PCMContainer): String = {
-      var json = ""
+    container.getPcm match {
+      case pcm: PCMImpl =>
+        val metadata = container.getMetadata
 
-      if (container.getPcm().isInstanceOf[PCMImpl]) {
+        // Convert all strings to base64 to avoid encoding problems
+        if (base64Encoding) {
+          encoder.encode(pcm)
+        }
 
-          val pcm = container.getPcm().asInstanceOf[PCMImpl]
+        // Serialize PCM
+        val kPcm = pcm.getKpcm()
+        val jsonPCM = Json.parse(serializer.serialize(kPcm))
 
-          // Convert all strings to base64 to avoid encoding problems
-          if (base64Encoding) {
-              encoder.encode(pcm)
-          }
+        // Decode PCM
+        if (base64Encoding) {
+          encoder.decode(pcm)
+        }
 
-          // Serialize PCM
-          val kPcm = pcm.getKpcm()
-          json = serializer.serialize(kPcm)
+        // Serialize Metadata
 
-          // Decode PCM
-          if (base64Encoding) {
-              encoder.decode(pcm)
-          }
-      }
+        // Serialize product positions
+        val productPositions = JsArray(for (product <- pcm.getProducts) yield {
+          val productId = product.asInstanceOf[ProductImpl].getkProduct().getGenerated_KMF_ID()
+          val position = metadata.getProductPosition(product)
+          JsObject(Seq(
+            "product" -> JsString(productId),
+            "position" -> JsNumber(position)
+          ))
+        })
 
-      val jsonPCM = Json.parse(json)
+        // Serialize feature positions
+        val featurePositions = JsArray(for (feature <- pcm.getConcreteFeatures) yield {
+          val featureId = feature.asInstanceOf[FeatureImpl].getkFeature().getGenerated_KMF_ID()
+          val position = metadata.getFeaturePosition(feature)
+          JsObject(Seq(
+            "feature" -> JsString(featureId),
+            "position" -> JsNumber(position)
+          ))
+        })
 
-      Json.stringify(JsObject(Map(
+        val jsonMetadata = JsObject(Seq(
+          "productPositions" -> productPositions,
+          "featurePositions" -> featurePositions,
+          "source" -> JsString(metadata.getSource),
+          "license" -> JsString(metadata.getLicense),
+          "creator" -> JsString(metadata.getCreator)
+        ))
+
+
+        Json.prettyPrint(JsObject(Map(
           "pcm" -> jsonPCM,
-          "metadata" -> JsObject(Map.empty[String, JsValue])
-      )))
+          "metadata" -> jsonMetadata
+        )))
+
+      case _ => ""
+    }
   }
 }
--- org.opencompare/api-java-impl/src/main/scala/org/opencompare/api/java/impl/io/KMFJSONLoader.scala
+++ org.opencompare/api-java-impl/src/main/scala/org/opencompare/api/java/impl/io/KMFJSONLoader.scala
@@ -1,14 +1,12 @@
 package org.opencompare.api.java.impl.io
 
-import java.io.File
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
+import java.io.{File, FileInputStream}
 
-import org.opencompare.api.java.impl.PCMImpl
+import org.opencompare.api.java.impl.{FeatureImpl, ProductImpl, PCMImpl}
 import org.opencompare.api.java.io.PCMLoader
 import org.opencompare.api.java.{PCMContainer, PCMMetadata}
 import org.opencompare.model.pcm.factory.DefaultPcmFactory
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json._
 
 import scala.collection.JavaConversions._
 
@@ -26,32 +24,88 @@ class KMFJSONLoader(val base64Decoding: Boolean = true) extends PCMLoader {
   }
 
   override def load(json: String): java.util.List[PCMContainer] = {
-    val jsonPCMContainer = Json.parse(json)
-
-    jsonPCMContainer match {
-      case JsObject(elements) =>
-        val jsonPCM = elements("pcm")
-        val jsonMetadata = elements("metadata")
-        val containers = loader.loadModelFromString(Json.stringify(jsonPCM)).toList
-        load(containers)
-      case _ => Nil
-    }
+    val jsonPCMContainer = Json.parse(json).as[JsObject]
+    load(jsonPCMContainer)
   }
 
   override def load(file: File): java.util.List[PCMContainer] = {
-    val bytes = Files.readAllBytes(file.toPath())
-    val json = new String(bytes, StandardCharsets.UTF_8)
-    load(json)
+    val inputStream = new FileInputStream(file)
+    val jsonPCMContainer = Json.parse(inputStream).as[JsObject]
+    inputStream.close()
+    load(jsonPCMContainer)
   }
 
-  private def load(containers : List[org.kevoree.modeling.api.KMFContainer] ) : List[PCMContainer] = {
+  private def load(jsonPCMContainer : JsObject) : List[PCMContainer] = {
+    val jsonPCM = jsonPCMContainer.value("pcm").as[JsObject]
+    val jsonMetadata = jsonPCMContainer.value("metadata").as[JsObject]
+
+
+    val containers = loader.loadModelFromString(Json.stringify(jsonPCM)).toList
+
     for (container <- containers) yield {
+      val containerPCM = new PCMContainer()
+
+      // Load PCM
       val pcm = new PCMImpl(container.asInstanceOf[org.opencompare.model.PCM])
       encoder.decode(pcm)
-      val containerPCM = new PCMContainer()
-      val metadata = new PCMMetadata(pcm)
       containerPCM.setPcm(pcm)
+
+      // Load metadata
+      val metadata = new PCMMetadata(pcm)
       containerPCM.setMetadata(metadata)
+
+
+      val source = jsonMetadata.value.get("source")
+      if (source.isDefined) {
+        metadata.setSource(source.get.as[String])
+      }
+
+      val license = jsonMetadata.value.get("license")
+      if (license.isDefined) {
+        metadata.setLicense(license.get.as[String])
+      }
+
+      val creator = jsonMetadata.value.get("creator")
+      creator match {
+        case Some(JsString(value)) => metadata.setCreator(value)
+        case _ =>
+      }
+
+
+      val jsonProductPositions = jsonMetadata.value("productPositions").as[JsArray]
+      val jsonFeaturePositions = jsonMetadata.value("featurePositions").as[JsArray]
+
+      for (jsonProductPosition <- jsonProductPositions.value) {
+        val jsonPos = jsonProductPosition.as[JsObject].value
+        val productName = jsonPos("product").as[JsString].value
+        val position = jsonPos("position").as[JsNumber].value.toIntExact
+
+        val product = pcm.getProducts.find { p =>
+          val id = p.asInstanceOf[ProductImpl].getkProduct().getGenerated_KMF_ID()
+          id == productName
+        }
+        if (product.isDefined) {
+          metadata.setProductPosition(product.get, position)
+        }
+
+      }
+
+      for (jsonFeaturePosition <- jsonFeaturePositions.value) {
+        val jsonPos = jsonFeaturePosition.as[JsObject].value
+        val featureName = jsonPos("feature").as[JsString].value
+        val position = jsonPos("position").as[JsNumber].value.toIntExact
+
+        val feature = pcm.getConcreteFeatures.find { f =>
+          val id = f.asInstanceOf[FeatureImpl].getkFeature().getGenerated_KMF_ID()
+          id == featureName
+        }
+        if (feature.isDefined) {
+          metadata.setFeaturePosition(feature.get, position)
+        }
+
+      }
+
+
       containerPCM
     }
   }
