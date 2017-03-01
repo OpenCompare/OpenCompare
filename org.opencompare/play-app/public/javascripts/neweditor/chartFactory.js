4d9baf6ba2732e47969b19e4a6d2fcaa92e8cfcf
Olivier Pelgrin <olivier.pelgrin@etudiant.univ-rennes1.fr>
2017-03-01 15:56:57 +0100

    typos

--- org.opencompare/play-app/public/javascripts/neweditor/chartFactory.js
+++ org.opencompare/play-app/public/javascripts/neweditor/chartFactory.js
@@ -168,43 +168,43 @@ ChartFactory.prototype.drawPie = function(){
         }
       }
     };
-	
+
     var feat = this.chartDataX;
-	
+
     // create two arrays
 	var arr = [0];
 	var arr2 = [0];
-	
+
 	// for each product
     for(var p in this.editor.products){
-		
+
 	  var product = this.editor.products[p];
 	  // we see if the product is visible
 	  if(product.visible) {
-		
+
 		// we recover the value of the product and parse in int
 		var label = product.getCell(this.editor.features[0]).content;
 		var value = parseFloat(product.getCell(feat).content);
-		
+
 		// push only if the value is numerical value
 		if (!isNaN(value)){
 			this.chartData.data.datasets[0].data.push(value);
-			
-			// we create a map, in the first array is the values 
+
+			// we create a map, in the first array is the values
 			// and the labels is in the second array with the same index
 			arr.push(parseFloat(product.getCell(feat).content));
 			arr2.push(label);
 		}
-		
+
 	  }
     }
-	
+
 	// we sort directly the array of number
 	this.chartData.data.datasets[0].data=this.chartData.data.datasets[0].data.sort((a,b)=>a-b);
-	
+
 	var i = 0;
 	while (i < this.chartData.data.datasets[0].data.length) {
-		
+
 		// we recover the value of the first case in the array data
 		var nb = this.chartData.data.datasets[0].data[i];
 		// we recover the index in the first array with the value
@@ -216,7 +216,7 @@ ChartFactory.prototype.drawPie = function(){
 		this.chartData.data.labels.push(label);
 		// we add a color thanks the label
 		this.chartData.data.datasets[0].backgroundColor.push(label.toColour());
-		
+
 		i++;
 	}
 
@@ -426,10 +426,10 @@ String.prototype.toColour = function () {
 ChartFactory.prototype.update = function(){
   if(this.chart != null){
     var that = this;
-    if(this.timout != null) {
+    if(this.timeout != null) {
       clearTimeout(this.timeout);
     }
-    this.timout = setTimeout(function(){
+    this.timeout = setTimeout(function(){
       that.drawChart();
     },500);
   }
